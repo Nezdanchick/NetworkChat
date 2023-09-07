194e285a3f6362b5bbf787e8250f6cc1e9d34d7e
Nezdanchick <Win10Artem@gmail.com>
2023-09-07 11:07:36 +0400

    finished chat realization

--- NetworkChat/Chat.Designer.cs
+++ NetworkChat/Chat.Designer.cs
@@ -31,26 +31,76 @@
             components = new System.ComponentModel.Container();
             timer = new System.Windows.Forms.Timer(components);
             labelChat = new Label();
+            panel = new Panel();
+            richTextBoxMessage = new RichTextBox();
+            buttonAdd = new Button();
+            buttonSend = new Button();
+            panel.SuspendLayout();
             SuspendLayout();
             // 
             // labelChat
             // 
             labelChat.AutoSize = true;
-            labelChat.Location = new Point(12, 9);
+            labelChat.Dock = DockStyle.Fill;
+            labelChat.Location = new Point(0, 0);
             labelChat.Name = "labelChat";
             labelChat.Size = new Size(70, 20);
             labelChat.TabIndex = 0;
             labelChat.Text = "Chat Text";
+            // 
+            // panel
+            // 
+            panel.BackColor = Color.Silver;
+            panel.Controls.Add(richTextBoxMessage);
+            panel.Controls.Add(buttonAdd);
+            panel.Controls.Add(buttonSend);
+            panel.Dock = DockStyle.Bottom;
+            panel.Location = new Point(0, 530);
+            panel.Name = "panel";
+            panel.Size = new Size(914, 70);
+            panel.TabIndex = 1;
+            // 
+            // richTextBoxMessage
+            // 
+            richTextBoxMessage.Dock = DockStyle.Fill;
+            richTextBoxMessage.Location = new Point(90, 0);
+            richTextBoxMessage.Name = "richTextBoxMessage";
+            richTextBoxMessage.Size = new Size(734, 70);
+            richTextBoxMessage.TabIndex = 2;
+            richTextBoxMessage.Text = "";
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.Dock = DockStyle.Left;
+            buttonAdd.Location = new Point(0, 0);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(90, 70);
+            buttonAdd.TabIndex = 1;
+            buttonAdd.Text = "Add";
+            buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonSend
+            // 
+            buttonSend.Dock = DockStyle.Right;
+            buttonSend.Location = new Point(824, 0);
+            buttonSend.Name = "buttonSend";
+            buttonSend.Size = new Size(90, 70);
+            buttonSend.TabIndex = 0;
+            buttonSend.Text = "Send";
+            buttonSend.UseVisualStyleBackColor = true;
+            buttonSend.Click += ButtonSend_Click;
             // 
             // Chat
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(914, 600);
+            Controls.Add(panel);
             Controls.Add(labelChat);
             Margin = new Padding(3, 4, 3, 4);
             Name = "Chat";
             Text = "Chat";
+            panel.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -58,5 +108,9 @@
         #endregion
         private System.Windows.Forms.Timer timer;
         private Label labelChat;
+        private Panel panel;
+        private Button buttonAdd;
+        private Button buttonSend;
+        private RichTextBox richTextBoxMessage;
     }
 }
--- NetworkChat/Chat.cs
+++ NetworkChat/Chat.cs
@@ -10,12 +10,22 @@ namespace NetworkChat
         public Chat()
         {
             InitializeComponent();
+            Text = $"{User.Name} {User.Server.EndPoint}";
+
             labelChat.Text = string.Empty;
             _chatLineHeight = labelChat.Height;
 
             timer.Interval = 100;
             timer.Tick += Timer_Tick;
             timer.Start();
+
+            User.Server.BroadcastAsync();
+        }
+        private void ButtonSend_Click(object sender, EventArgs e)
+        {
+            var message = richTextBoxMessage.Text;
+            if (message.Trim().Length > 0)
+                User.Client.Send((User.Name, message));
         }
         private void Timer_Tick(object? sender, EventArgs e)
         {
@@ -23,7 +33,7 @@ namespace NetworkChat
             {
                 timer.Stop();
                 MessageBox.Show(
-                    "Сheck the connection to the server try again",
+                    "Сheck the connection to the server and try again",
                     "Disconnected from server",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Error);
@@ -33,9 +43,10 @@ namespace NetworkChat
         }
         private void UpdateChat()
         {
-            UserSay("Nezdanchick", "lol");
+            var (name, message) = User.Client.Receive<(string, string)>();
+            if (name != null && message != null)
+                UserSay(name, message);
         }
-
         private void UserSay(string username, string text)
         {
             labelChat.Text += $"{username}: {text}\n";
@@ -44,7 +55,7 @@ namespace NetworkChat
         }
         private void ShrinkText()
         {
-            int borders = (_chatLineHeight + labelChat.Top) * 2;
+            int borders = (_chatLineHeight + labelChat.Top) * 2 + panel.Height;
             int count = _chatMessageCount - (Height - borders) / _chatLineHeight;
             if (count <= 0)
                 return;
